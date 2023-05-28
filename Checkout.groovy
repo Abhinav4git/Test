7e6667cfc9b39d006e1e7bf33f8a4ea8342b230a
Abhinav4git <57993091+Abhinav4git@users.noreply.github.com>
2023-05-29 07:58:39 +0800

    Update Checkout.groovy

--- Checkout.groovy
+++ Checkout.groovy
@@ -24,3 +24,20 @@ pipeline {
 
 Save and commit the Jenkinsfile in your Bitbucket repository. When you run the Jenkins pipeline, it will clone the specified Bitbucket repository and checkout the desired branch or commit for further processing.
 Note that you may need to configure additional parameters or options based on your specific requirements, such as branch filtering, pull requests, etc. Consult the Bitbucket Branch Source Plugin documentation for more advanced usage.//
+    
+    
+    In a Jenkinsfile, you can use the dir step to change the current directory of the Pipeline.
+    This can be useful in cases where you need to change directories to run a specific command or access files in a different location. Here's an example usage:
+
+pipeline {
+    agent any
+    stages {
+        stage('Change Directory') {
+            steps {
+                dir('my_directory') {
+                    sh 'ls -la'
+                }
+            }
+        }
+    }
+}
