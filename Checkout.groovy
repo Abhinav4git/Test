5e673cdeb702b064f75fbb6358ddf85b07d87cca
Abhinav4git <57993091+Abhinav4git@users.noreply.github.com>
2023-05-28 18:34:25 +0800

    Add files via upload

---
+++ Checkout.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout([
+                    $class: 'BitbucketSCM',
+                    credentialsId: 'your_credentials_id',
+                    cloneType: 'ssh',
+                    extensions: [[$class: 'CleanBeforeCheckout']],
+                    repositoryUrl: 'git@bitbucket.org:your_username/your_repository.git',
+                    ref: 'master'  // or specify the branch or commit you want to check out
+                ])
+            }
+        }
+
+        // Add more stages for your build, test, deploy, etc.
+    }
+}
+
+
+//In the above example, replace 'your_credentials_id' with the ID of the Bitbucket credentials you've configured in Jenkins. Replace 'your_username/your_repository.git' with the actual repository URL you want to search for.
+
+Save and commit the Jenkinsfile in your Bitbucket repository. When you run the Jenkins pipeline, it will clone the specified Bitbucket repository and checkout the desired branch or commit for further processing.
+Note that you may need to configure additional parameters or options based on your specific requirements, such as branch filtering, pull requests, etc. Consult the Bitbucket Branch Source Plugin documentation for more advanced usage.//
---
+++ Find.groovy
@@ -0,0 +1,37 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Search Files') {
+            steps {
+                script {
+                    // Define the directory where you want to search for files
+                    def searchDirectory = '/path/to/search/directory'
+                    
+                    // Define the extensions you want to search for
+                    def fileExtensions = ['.txt', '.csv', '.log']
+                    
+                    // Search for files with the specified extensions
+                    def foundFiles = findFiles(glob: "${searchDirectory}/**/*.{${fileExtensions.join(',')}}")
+                    
+                    // Print the found file paths
+                    foundFiles.each { file ->
+                        println "Found file: ${file}"
+                    }
+                    
+                    // You can perform further actions with the found files, such as processing or copying them
+                }
+            }
+        }
+    }
+}
+
+
+
+//In this example, the findFiles step is used to search for files in the specified directory (searchDirectory) with the given extensions (fileExtensions). The glob parameter allows you to specify a pattern using the {} syntax to match multiple file extensions. The foundFiles variable stores the paths of the found files.
+
+You can customize the searchDirectory and fileExtensions variables to match your specific requirements. Additionally, you can add more stages or steps to perform further actions with the found files, such as processing or copying them.
+
+Make sure to replace /path/to/search/directory with the actual directory path you want to search in.//
+
+
